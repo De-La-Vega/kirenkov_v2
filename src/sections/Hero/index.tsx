acf3d4c68df3df641c4f1962eec40a16217b15b3
Vitaliy Kirenkov <kirenkov_vitaliy@yahoo.com>
2024-04-08 12:13:54 +0400

    added helmet

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import Typed from 'typed.js';
+import { Helmet } from 'react-helmet';
 
 import { FollowMe } from '../../components/FollowMe';
 import { WriteToMe } from '../../components/WriteToMe';
@@ -32,27 +33,34 @@ export const Hero: React.FC = () => {
     }, [t]);
 
     return (
-        <section className="g-outer section-hero">
-            <div className="g-inner">
-                <div className="section-hero__content-left">
-                    <div className="section-hero__position"><span ref={elTyped} /></div>
-                    <div className="section-hero__name">{t('hero.name')}</div>
-                    <div className="section-hero__follow-me"><FollowMe /></div>
-                    <div className="section-hero__write-to-me"><WriteToMe /></div>
-                </div>
-                <div className="section-hero__content-right">
-                    <picture>
-                        <source srcSet={`${hero_large} 800w, ${hero_small} 300w`} sizes="(min-width: 62em) 800px, (max-width: 47.9375em) 300px" type="image/webp" />
-                        <source srcSet={`${hero_large_fallback} 800w, ${hero_small_fallback} 300w`} sizes="(min-width: 62em) 800px, (max-width: 47.9375em) 300px" type="image/png" />
-                        <img className="section-hero__img" src={hero_large_fallback} alt="Vitaliy Kirenkov" width="800" height="1000" />
-                    </picture>
+        <>
+            <Helmet>
+                <link rel="preload" as="image" href={hero_large} />
+                <link rel="preload" as="image" href={hero_small} />
+            </Helmet>
 
-                    <div className="section-hero__columns">
-                        <div></div>
-                        <div></div>
+            <section className="g-outer section-hero">
+                <div className="g-inner">
+                    <div className="section-hero__content-left">
+                        <div className="section-hero__position"><span ref={elTyped} /></div>
+                        <div className="section-hero__name">{t('hero.name')}</div>
+                        <div className="section-hero__follow-me"><FollowMe /></div>
+                        <div className="section-hero__write-to-me"><WriteToMe /></div>
+                    </div>
+                    <div className="section-hero__content-right">
+                        <picture>
+                            <source srcSet={`${hero_large} 800w, ${hero_small} 300w`} sizes="(min-width: 62em) 800px, (max-width: 47.9375em) 300px" type="image/webp" />
+                            <source srcSet={`${hero_large_fallback} 800w, ${hero_small_fallback} 300w`} sizes="(min-width: 62em) 800px, (max-width: 47.9375em) 300px" type="image/png" />
+                            <img className="section-hero__img" src={hero_large_fallback} alt="Vitaliy Kirenkov" width="800" height="1000" />
+                        </picture>
+
+                        <div className="section-hero__columns">
+                            <div></div>
+                            <div></div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 };
